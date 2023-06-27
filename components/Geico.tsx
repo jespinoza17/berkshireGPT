0836fd8d81641b6940e9bb4e13439c7780118947
Juan Espinoza <jaespin300@yahoo.com>
2023-06-27 03:14:25 -0700

    added Geico ad

---
+++ components/Geico.tsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx';
+
+export const Geico = () => (
+  <div className="flex flex-col items-center">
+    <a href="https://www.geico.com/" className="my-2">
+      <img src="https://www.berkshirehathaway.com/geicoimg.gif" />
+    </a>
+    <div className="text-center text-xs">
+      <p>FOR A FREE CAR INSURANCE RATE QUOTE THAT COULD SAVE YOU SUBSTANTIAL MONEY</p>
+      <p><a href="https://www.geico.com/">WWW.GEICO.COM</a> OR CALL 1-888-395-6349, 24 HOURS A DAY</p>
+    </div>
+  </div>
+);
